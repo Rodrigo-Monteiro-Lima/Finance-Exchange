f28dc7dab1ee4356dee8a221002ab5315df77ab4
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-31 20:27:47 -0300

    feat: table gets info from globalstate and render in the screen

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './Table.css';
 // import { MdDeleteForever } from 'react-icons/md';
@@ -7,6 +7,7 @@ import './Table.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -23,11 +24,64 @@ class Table extends Component {
           </tr>
         </thead>
         <tbody>
-          {}
+          {expenses && expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                {Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}
+                {/* .toLocaleString(
+                    'pt-BR',
+                    { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' },
+                  ) */}
+
+              </td>
+              <td>
+                {Number(expense.value
+                  * expense.exchangeRates[expense.currency].ask).toFixed(2)}
+                {/* .toLocaleString(
+                    'pt-BR',
+                    { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' },
+                  )  */}
+              </td>
+              <td>Real</td>
+              <td>
+                {/* <FaRegEdit data-testid="edit-btn" /> */}
+                <button
+                  type="button"
+                  data-testid="edit-btn"
+                >
+                  Editar
+
+                </button>
+                {/* <MdDeleteForever
+                  data-testid="delete-btn"
+                  onClick={ () => removeExpense(index) }
+                /> */}
+                <button
+                  type="button"
+                  data-testid="delete-btn"
+                >
+                  Excluir
+
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     );
   }
 }
 
-export default connect()(Table);
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape().isRequired).isRequired,
+};
+
+const mapStateToProps = ({ wallet: { expenses } }) => ({
+  expenses,
+});
+export default connect(mapStateToProps)(Table);
